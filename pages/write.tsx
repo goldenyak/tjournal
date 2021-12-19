fb43797330ccfb0f792cded8790f2bcf248762cb
goldenyak <goldenyakegor@gmail.com>
2021-12-19 22:38:42 +0300

    add some functions

---
+++ pages/write.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {NextPage} from "next";
+import {TextField} from '@material-ui/core';
+
+interface WritePageProps {
+
+}
+
+const WritePage: NextPage = () => {
+    return (
+        <TextField/>
+    );
+};
+
+export default WritePage;
+
