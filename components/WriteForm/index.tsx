16a812bf011606bd345e9ae029b3ffd5a058f579
goldenyak <goldenyakegor@gmail.com>
2021-12-20 00:13:35 +0300

    add some functions

--- components/WriteForm/index.tsx
+++ components/WriteForm/index.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import {Input} from "@material-ui/core";
-import styles from './WriteForm.module.scss'
+import styles from './WriteForm.module.scss';
+import EditorJS from '@editorjs/editorjs';
 
 interface WriteFormProps {
     title?: string;
 }
 
 const WriteForm: React.FC<WriteFormProps> = ({title}) => {
+
+    React.useEffect(() => {
+        const editor = new EditorJS({
+            holder: 'editor'
+        });
+    }, [])
+
     return (
         <div>
             <Input classes={{root: styles.titleField}} placeholder='Заголовок' defaultValue={title}/>
+            <div id='editor'>
+
+            </div>
         </div>
     );
 };
--- pages/write.tsx
+++ pages/write.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import {NextPage} from "next";
-import {TextField} from '@material-ui/core';
 import {MainLayout} from "../layouts/MainLayout";
 import WriteForm from "../components/WriteForm";
+
 
 interface WritePageProps {
 
