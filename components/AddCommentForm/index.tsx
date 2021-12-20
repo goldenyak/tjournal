254d7f687cb2059fa5454d0f763be867c6114b09
goldenyak <goldenyakegor@gmail.com>
2021-12-20 23:46:47 +0300

    add some functions

--- components/AddCommentForm/index.tsx
+++ components/AddCommentForm/index.tsx
@@ -3,9 +3,17 @@ import {Input} from "@material-ui/core";
 import styles from './AddCommentForm.module.scss'
 
 const AddCommentForm: React.FC = () => {
+
+    const [clicked, setClicked] = React.useState(false)
+
     return (
         <div>
-            <Input classes={{root: styles.fieldRoot}} fullWidth placeholder="Написать комментарий ..."/>
+            <Input onFocus={() => setClicked(true)}
+                   classes={{root: styles.fieldRoot}}
+                   minRows={clicked ? 5 : 2}
+                   multiline
+                   fullWidth
+                   placeholder="Написать комментарий ..."/>
         </div>
     );
 };
