5708861ed354e2461a99d6e2e0fac240a1c6d295
goldenyak <goldenyakegor@gmail.com>
2021-12-22 00:05:50 +0300

    add some functions

---
+++ data.js
@@ -0,0 +1,54 @@
+
+  module.exports = {
+    "comments": {
+      "popular": [{
+        createdAt: new Date().toString(),
+        id: 1,
+        user: {
+          fullname: 'Вася Пупкин',
+          avatarUrl: 'https://images.unsplash.com/photo-1639929781092-fe59fae895c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMTF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+        },
+        text: 'Теперь, каждое рабочее утро, после кровати, я перекладываюсь туда спать ещё на часок. Ну и…',
+        post: {
+          title: 'Какая у вас дома ванна?',
+        },
+      },
+        {
+          createdAt: new Date().toString(),
+          id: 2,
+          user: {
+            fullname: 'Вася Пупкин',
+            avatarUrl: 'https://images.unsplash.com/photo-1639953610946-5871190d0162?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60'
+          },
+          text: 'Теперь, каждое рабочее утро, после кровати, я перекладываюсь туда спать ещё на часок. Ну и…',
+          post: {
+            title: 'Какая у вас дома ванна?',
+          },
+        },
+        {
+          createdAt: new Date().toString(),
+          id: 3,
+          user: {
+            fullname: 'Вася Пупкин',
+            avatarUrl: 'https://images.unsplash.com/photo-1640003854809-b2a0e97bb289?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyM3x8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60'
+          },
+          text: 'Теперь, каждое рабочее утро, после кровати, я перекладываюсь туда спать ещё на часок. Ну и…',
+          post: {
+            title: 'Какая у вас дома ванна?',
+          },
+        }
+      ],
+      "new": [{
+          createdAt: new Date().toString(),
+          id: 1,
+          user: {
+              fullname: 'Вася Пупкин',
+              avatarUrl: 'https://images.unsplash.com/photo-1639929781092-fe59fae895c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMTF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+          },
+          text: 'Теперь, каждое рабочее утро, после кровати, я перекладываюсь туда спать ещё на часок. Ну и…',
+          post: {
+              title: 'Какая у вас дома ванна?',
+          },
+      }]
+    }
+  }
