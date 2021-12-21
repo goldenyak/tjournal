5b10e1cd841196e0f1ad459f1f1ab1ab06ca4fb7
goldenyak <goldenyakegor@gmail.com>
2021-12-21 23:37:55 +0300

    add some functions

--- components/AddCommentForm/index.tsx
+++ components/AddCommentForm/index.tsx
@@ -4,17 +4,26 @@ import styles from './AddCommentForm.module.scss'
 
 const AddCommentForm: React.FC = () => {
 
-    const [clicked, setClicked] = React.useState(false)
+    const [clicked, setClicked] = React.useState(false);
+    const [text, setText] = React.useState('');
+
+    const onAddComment = () => {
+        setClicked(false);
+        setText('');
+
+    }
 
     return (
         <div className={styles.form}>
             <Input onFocus={() => setClicked(true)}
+                   onChange={e => setText(e.currentTarget.value)}
+                   value={text}
                    classes={{root: styles.fieldRoot}}
                    minRows={clicked ? 5 : 2}
                    multiline
                    fullWidth
                    placeholder="Написать комментарий ..."/>
-            {clicked && <Button className={styles.addButton} variant='contained' color="primary">Добавить</Button>}
+            {clicked && <Button onClick={() => onAddComment} className={styles.addButton} variant='contained' color="primary">Добавить</Button>}
         </div>
     );
 };
