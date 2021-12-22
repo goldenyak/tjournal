725e34f170515250b2aecc8fc496fe9c50a6d0af
goldenyak <goldenyakegor@gmail.com>
2021-12-22 22:06:31 +0300

    add some functions

--- components/SideComments/index.tsx
+++ components/SideComments/index.tsx
@@ -4,6 +4,7 @@ import ArrowRightIcon from '@material-ui/icons/NavigateNextOutlined';
 import styles from './SideComments.module.scss';
 import data from '../../data';
 import CommentItem from "../CommentItem/CommentItem";
+import clsx from "clsx";
 
 export const comments = [
     {
@@ -55,7 +56,7 @@ export const SideComments = () => {
     };
 
     return (
-        <div className={styles.root}>
+        <div className={clsx(styles.root, !visible && styles.rotated)}>
             <h3 onClick={toggleVisible}>
                 Комментарии <ArrowRightIcon/>
             </h3>
