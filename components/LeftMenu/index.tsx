221b96165fc9a001178e5deb9b260edf39d057e5
goldenyak <goldenyakegor@gmail.com>
2021-12-22 22:40:33 +0300

    add some functions

--- components/LeftMenu/index.tsx
+++ components/LeftMenu/index.tsx
@@ -31,7 +31,7 @@ export const LeftMenu: React.FC = () => {
           <li key={obj.path}>
             <Link href={obj.path}>
               <a>
-                <Button variant={router.asPath === obj.path ? 'contained' : 'default'}>
+                <Button variant={router.asPath === obj.path ? 'contained' : 'text'}>
                   {obj.icon}
                   {obj.text}
                 </Button>
