1d764771b4b612596fe1595be300a9ad12f1d366
goldenyak <goldenyakegor@gmail.com>
2022-01-14 22:18:21 +0300

    add some functions

--- components/AuthDialog/AuthDialog.tsx
+++ components/AuthDialog/AuthDialog.tsx
@@ -3,6 +3,7 @@ import {Button, Dialog, DialogContent, DialogContentText, TextField, Typography}
 import ArrowBack from '@material-ui/icons/ArrowBack';
 import styles from './AuthDialog.module.scss';
 import {MainForm} from "./Form/Main";
+import LoginForm from "./Form/Login";
 
 interface AuthDialogProps {
     onClose: () => void,
@@ -24,48 +25,7 @@ export const AuthDialog: React.FC<AuthDialogProps> = ({onClose, visible}) => {
                 <DialogContentText>
                     <div className={styles.content}>
                         {formType === "main" && <MainForm onOpenLogin={() => setFormType("login")}/>}
-                        {formType === "login" && <div>
-                            <div className={styles.backTitle} onClick={() => setFormType("main")}>
-                                <ArrowBack className={styles.backTitleIcon}/>
-                                <Typography  className={styles.backTitleText}>Вернуться</Typography>
-                            </div>
-                            <form>
-                                <TextField
-                                    className="mb-20"
-                                    size="small"
-                                    label="Имя и фамилия"
-                                    variant="outlined"
-                                    fullWidth
-                                    required
-                                />
-                                <TextField
-                                    className="mb-20"
-                                    size="small"
-                                    label="Почта"
-                                    type="email"
-                                    variant="outlined"
-                                    fullWidth
-                                    required
-                                />
-                                <TextField
-                                    size="small"
-                                    label="Пароль"
-                                    type="password"
-                                    variant="outlined"
-                                    fullWidth
-                                    required
-                                />
-                                <div className="d-flex align-center justify-between mt-20">
-                                    <Button color="primary" variant="contained">
-                                        Войти
-                                    </Button>
-                                    <Button color="primary" variant="text">
-                                        Регистрация
-                                    </Button>
-                                </div>
-
-                            </form>
-                        </div>}
+                        {formType === "login" && <LoginForm onRegister={() => setFormType("main")}/>}
                     </div>
                 </DialogContentText>
             </DialogContent>
