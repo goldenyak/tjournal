d486614586807865174bdaf5dfd845323e187bda
goldenyak <goldenyakegor@gmail.com>
2022-01-14 22:29:41 +0300

    add some functions

--- components/AuthDialog/AuthDialog.tsx
+++ components/AuthDialog/AuthDialog.tsx
@@ -4,6 +4,7 @@ import ArrowBack from '@material-ui/icons/ArrowBack';
 import styles from './AuthDialog.module.scss';
 import {MainForm} from "./Form/Main";
 import LoginForm from "./Form/Login";
+import {RegisterForm} from "./Form/Register";
 
 interface AuthDialogProps {
     onClose: () => void,
@@ -25,7 +26,8 @@ export const AuthDialog: React.FC<AuthDialogProps> = ({onClose, visible}) => {
                 <DialogContentText>
                     <div className={styles.content}>
                         {formType === "main" && <MainForm onOpenLogin={() => setFormType("login")}/>}
-                        {formType === "login" && <LoginForm onRegister={() => setFormType("main")}/>}
+                        {formType === "login" && <LoginForm onOpenRegister={() => setFormType("register")} onOpenLogin={() => setFormType("main")}/>}
+                        {formType === "register" && <RegisterForm onOpenLogin={() => setFormType("login")}/>}
                     </div>
                 </DialogContentText>
             </DialogContent>
