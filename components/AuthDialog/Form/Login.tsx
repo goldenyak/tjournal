48a5149026feb123c30387329f296f71f7bade39
goldenyak <goldenyakegor@gmail.com>
2022-01-14 23:25:44 +0300

    add some functions

--- components/AuthDialog/Form/Login.tsx
+++ components/AuthDialog/Form/Login.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
 import styles from "../AuthDialog.module.scss";
 import ArrowBack from "@material-ui/icons/ArrowBack";
 import {Button, TextField, Typography} from "@material-ui/core";
+import {LoginFormSchema} from "../../../utils/schemas/loginValidation";
 
 interface LoginFormProps {
     onOpenRegister: () => void
@@ -9,27 +13,44 @@ interface LoginFormProps {
 }
 
 const LoginForm: React.FC<LoginFormProps> = ({onOpenRegister, onOpenLogin}) => {
+
+    const form = useForm({
+        mode: "onChange",
+        resolver: yupResolver(LoginFormSchema)
+    })
+
+    const onSubmit = data => console.log(data);
+    console.log(form.formState.errors)
+
     return (
         <div>
             <div className={styles.backTitle} onClick={onOpenLogin}>
                 <ArrowBack className={styles.backTitleIcon}/>
                 <Typography  className={styles.backTitleText}>К авторизации</Typography>
             </div>
-            <form>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
                 <TextField
+                    {...form.register("email")}
+                    name="email"
                     className="mb-20"
                     size="small"
                     label="Почта"
                     type="email"
                     variant="outlined"
+                    error={!!form.formState.errors.email?.message}
+                    helperText={form.formState.errors.email?.message}
                     fullWidth
                     required
                 />
                 <TextField
+                    {...form.register("password")}
+                    name="password"
                     size="small"
                     label="Пароль"
                     type="password"
                     variant="outlined"
+                    error={!!form.formState.errors.password?.message}
+                    helperText={form.formState.errors.password?.message}
                     fullWidth
                     required
                 />
